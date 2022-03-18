3a9c57e297de563698ff2fe1c1dd9928025eee23
RileyMon <101885791+RileyMon@users.noreply.github.com>
2022-03-18 12:30:19 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 scene.setBackgroundColor(3)
-let mySprite = sprites.create(img`
+let hiAlexa = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . f . f . f . . 5 5 5 5 5 5 5 . 
     . f . f . f . . 5 5 f 5 f 5 5 . 
@@ -16,4 +16,22 @@ let mySprite = sprites.create(img`
     f . f . f . f . . f . f . f . f 
     f . f . f f f f . f . f . f . f 
     . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
+let alexa = sprites.create(img`
+    . . . . . f f f f f f . . . . . 
+    . . . . f e e e e e e . . . . . 
+    . . . f e e e e e e e e f . . . 
+    . . f e e e e e e e e e e f . . 
+    . . f e e e e e e e e e e f . . 
+    . f e e e e e d d e e e e e f . 
+    . f e e f f d d d d f f e e f . 
+    . f e e f b 6 d d 6 b f e e f . 
+    . f e e d 1 6 d d 6 1 d e e f . 
+    f f e e f d d d d d d f e e f f 
+    f e e f f f d d d d f f f e e f 
+    . f e e f b 7 7 7 7 b f e e f . 
+    . . e 4 c 7 7 7 7 7 7 c 4 e . . 
+    . . e f b 7 b 7 b 7 b b f e . . 
+    . . . f f 1 d 1 d 1 d f f . . . 
+    . . . . . f f b b f f . . . . . 
     `, SpriteKind.Player)
