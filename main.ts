8a62c56ba08af5633b60e88803b93b1b875b622a
RileyMon <101885791+RileyMon@users.noreply.github.com>
2022-03-18 12:26:07 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
 scene.setBackgroundColor(3)
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . f . f . f . . 5 5 5 5 5 5 5 . 
+    . f . f . f . . 5 5 f 5 f 5 5 . 
+    . f f f . f . . 5 5 5 5 5 5 5 . 
+    . f . f . f . . 5 f 5 5 5 f 5 . 
+    . f . f . f . . 5 f 5 5 5 f 5 . 
+    . f . f . f . . 5 5 f f f 5 5 . 
+    . . . . . . . . 5 5 5 5 5 5 5 . 
+    . . . . . . . . . . . . . . . . 
+    f f f . f . f f . f . f . f f f 
+    f . f . f . f . . f . f . f . f 
+    f . f . f . f . . . f . . f . f 
+    f f f . f . f f . . f . . f f f 
+    f . f . f . f . . f . f . f . f 
+    f . f . f f f f . f . f . f . f 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
